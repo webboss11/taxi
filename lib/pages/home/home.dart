0792974459f13499794ecc4ff3ecb43f98b2d199
webboss11 <159762418+webboss11@users.noreply.github.com>
2024-08-18 19:48:04 +0000

    Implemented SharECar features

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,8 +1,8 @@
-import 'package:citycab/models/user.dart';
-import 'package:citycab/pages/auth/auth_page.dart';
-import 'package:citycab/pages/map/map_view.dart';
-import 'package:citycab/repositories/user_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:sharecar/models/user.dart';
+import 'package:sharecar/pages/auth/auth_page.dart';
+import 'package:sharecar/pages/map/map_view.dart';
+import 'package:sharecar/repositories/user_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,20 +17,69 @@ class HomePage extends StatelessWidget {
           valueListenable: UserRepository.instance.userNotifier,
           builder: (context, value, child) {
             if (value != null) {
-              return Builder(
-                builder: (context) {
-                  if (value.isVerified) {
-                    return MapViewWidget();
-                  } else {
-                    return AuthPageWidget(page: 2, uid: value.uid);
-                  }
-                },
-              );
+              return value.isVerified ? _buildRideSearch(context) : AuthPageWidget(page: 2, uid: value.uid);
             } else {
               return AuthPageWidget(page: 0);
             }
           },
         ),
+      ),
+    );
+  }
+
+  Widget _buildRideSearch(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Current Location',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 16.0),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Destination',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 16.0),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Date & Time',
+              border: OutlineInputBorder(),
+            ),
+            onTap: () async {
+              DateTime? date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2101),
+              );
+              if (date != null) {
+                // Handle the date selection
+              }
+            },
+          ),
+          SizedBox(height: 16.0),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Number of Passengers',
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              // Handle the search action
+            },
+            child: Text('Search'),
+          ),
+        ],
       ),
     );
   }
